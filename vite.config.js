c8c511582f32f5ce947f063b798ed31c7ee43c49
Arina Makhmudova <am@talentpowered.team>
2024-03-27 18:39:12 +0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 export default {
     // base url [prefix] (you need to configure this, if you want to run the editor out of a subdirectory of the domain)
     // either add the full url or the directory path like e.g. '/squidex-samples/editors/richtext'
-    base: "/",
+    base: "/richtext-editor",
 };
